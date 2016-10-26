135d613d764675afd817da094d918176f99b9b39
joranvar <bart.post@gmail.com>
2016-10-26 11:16:57 +0200

    Create domain types

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,12 @@
 -- | Starman - https://www.futurelearn.com/courses/functional-programming-haskell/1/steps/116475
 module Lib
   (
-    -- * Exported functions
+    -- * Domain types
+    Secret(..)
+  , Public(..)
+  , Guess(..)
   ) where
+
+newtype Secret = Secret { secret :: String }
+newtype Public = Public { public :: String }
+newtype Guess = Guess Char
